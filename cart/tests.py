70ad0e9985dfe51d2da3418df29b457311486425
sophie <1454067659@qq.com>
2022-11-30 23:24:03 +1100

    final part of the project

--- cart/tests.py
+++ cart/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
+import json, requests
+
+from goods.models import Goods
+from requests import Response
 
 # Create your tests here.
+class CartTestCase(TestCase):
+    BASE_URL = "http://127.0.0.1:8000/cart/cart_detail"
+    # def setUp(self):
+    #     goods_lst = [{"goods_name":"T-shirt", "goods_price":30.99, "ship_from":"US", "weight":0.2},
+    #                 {"goods_name":"Blouse", "goods_price":10.99, "ship_from":"UK", "weight":0.3},
+    #                 {"goods_name":"Pants", "goods_price":64.99, "ship_from":"UK", "weight":0.9},
+    #                 {"goods_name":"Sweatpants", "goods_price":84.99, "ship_from":"CN", "weight":1.1},
+    #                 {"goods_name":"Jacket", "goods_price":199.99, "ship_from":"US", "weight":2.2},
+    #                 {"goods_name":"Shoes", "goods_price":79.99, "ship_from":"CN", "weight":1.3}]
+    #     for goods in goods_lst:
+    #         Goods.objects.create(**goods)
+    def get(self):
+        params = {
+            "user_id":2
+        }
+        ret = requests.get(self.BASE_URL, params=params)
+        self.result = (json.loads(ret.content), ret.status_code)
+
+    def test_get_goods_in_cart(self):
+        self.get()
+        self.assertEqual(200, self.result[1])
+        errmsg = self.result[0].get("errmsg")
+        self.assertEqual('$386.95', errmsg.get("Subtotal"))
+        self.assertEqual('$110.0', errmsg.get("Shipping"))
+        self.assertEqual('$54.173', errmsg.get("VAT"))
+        self.assertEqual('$433.129', errmsg.get("Total"))
+        self.assertEqual('-$7.999', errmsg.get("Discounts").get("10% off shoes"))
+        self.assertEqual('-$99.995', errmsg.get("Discounts").get("50% off jacket"))
+        self.assertEqual('-$10', errmsg.get("Discounts").get("$10 off shipping"))
+
+
--- cart/views.py
+++ cart/views.py
@@ -8,25 +8,45 @@ from goods.models import Goods
 from utils.constants import SHIPPING_RATE
 
 # Create your views here.
+#to calculate the discount
+
 
 class CartAPIView(APIView):
+    @staticmethod
+    def discount_oper(invoice_info, goods_total_amount, has_shoes, has_jacket, top_outfit_amount, discount, shoes_num):
+        #to calculate the discounts
+        if goods_total_amount >= 2:  # if goods amount is more than 2, get $10 off for shipping discount
+            discount += 10
+            invoice_info["Discounts"]["$10 off shipping"] = "-$10"
+        if has_shoes:  # shoes get 10% off for each pair
+            shoes_price = Goods.objects.filter(goods_name="Shoes").first().goods_price
+            discount += float(shoes_price) * 0.1 * shoes_num
+            invoice_info["Discounts"]["10% off shoes"] = f"-${float(shoes_price) * 0.1 * shoes_num}"
+        if top_outfit_amount >= 2 and has_jacket:  # 2 or more top outfit, get 50% off for one jacket
+            jacket_price = Goods.objects.filter(goods_name="Jacket").first().goods_price
+            discount += float(jacket_price) * 0.5
+            invoice_info["Discounts"]["50% off jacket"] = f"-${float(jacket_price) * 0.5}"
+        return discount
+
     def post(self, request):
         user_id = request.data.get("user_id")
-        goods_id = request.data.get("goods_id", None)
+        goods_id_lst = request.data.get("goods_id_lst", None)
         num = request.data.get("goods_number", 1)
         try:
-            the_goods = Goods.objects.get(pk=goods_id)
+            the_goods = Goods.objects.filter(pk__in=goods_id_lst).all()
         except:
             return Response({"errmsg": "the goods doesn't exist"}, status=status.HTTP_400_BAD_REQUEST)
         redis = get_redis_connection("cart")
-        original_num = redis.hget(f"cart_{user_id}", goods_id) #if the goods is already in cart
-        if not original_num:
-            original_num = 0
-        else:
-            original_num = int(original_num.decode())
-        redis.hset(f"cart_{user_id}", goods_id, original_num+num)
-        cart_total = redis.hlen(f"cart_{user_id}")
-        return Response({"errmsg": f"You have successfully added {the_goods.goods_name} in the cart!", "cart_total": cart_total},
+        print(">>", goods_id_lst)
+        for goods_id in goods_id_lst:
+            original_num = redis.hget(f"cart_{user_id}", goods_id) #if the goods is already in cart
+            if not original_num:
+                original_num = 0
+            else:
+                original_num = int(original_num.decode())
+            redis.hset(f"cart_{user_id}", goods_id, original_num+num)
+            cart_total = redis.hlen(f"cart_{user_id}")
+        return Response({"errmsg": f"You have successfully added {[g.goods_name for g in the_goods]} in the cart!", "cart_total": cart_total},
                         status=status.HTTP_201_CREATED)
 
     def get(self, request):
@@ -38,55 +58,42 @@ class CartAPIView(APIView):
         cart_hash = redis.hgetall(f"cart_{user_id}")
         if len(cart_hash) < 1:
             return Response({"error": "There is no goods in your cart"})
+        #get goods on the cart
+        # cart  = [(1, 1), (2, 1), (3, 1), (4, 1), (5, 1), (6, 2)]
         cart = [(int(key.decode()), int(value.decode())) for key, value in cart_hash.items()]
-        """
-        cart = [(1, 1), (2, 1), (3, 1), (4, 1), (5, 1), (6, 2)]
-        """
-        goods_total_amount = 0  #to record the total amount of the goods
+        goods_total_amount = 0          #to record the total amount of the goods
         top_outfit_amount  = 0          #to record the amount of the top outfit
-        total_price = 0         #to record the total price of all goods
-        has_jacket = 0          #if has jacket, change to 1
-        has_shoes = 0
-        shipping_total = 0
-        discount = 0
-        shoes_num = 0
+        total_price = 0                 #to record the total price of all goods
+        has_jacket = 0                  #if has jacket, change the value to 1
+        has_shoes = 0                   #if has shoes, change the value to 1
+        shipping_total = 0              #to record shipping fee(before discount)
+        discount = 0                    #to record discount
+        shoes_num = 0                   #to record shoes number
         invoice_info = {"Discounts":{}}
         for goods_id, goods_amount in cart:
-            goods_total_amount+=goods_amount          #总量
+            goods_total_amount+=goods_amount          #the amount of goods
             the_goods = Goods.objects.filter(pk=goods_id).first()
             the_shipping = SHIPPING_RATE.get(the_goods.ship_from)*the_goods.weight*goods_amount*10
             shipping_total += the_shipping
             if the_goods:
                 the_goods_price = the_goods.goods_price*goods_amount
-                total_price += the_goods_price  #打折前总价
-                if goods_id == 1 or goods_id == 2:   #如果上t-shirt或是blouse,记录
+                total_price += the_goods_price  #the price before discount
+                if the_goods.goods_name == "T-shirt" or the_goods.goods_name == "Blouse":   #如果上t-shirt或是blouse,记录
                     top_outfit_amount += goods_amount
-                if goods_id == 5:
+                if the_goods.goods_name == "Jacket":
                     has_jacket = 1
-                if goods_id == 6:
+                if the_goods.goods_name == "Shoes":
                     has_shoes = 1
                     shoes_num += 1
         VAT = round(float(total_price)*0.14, 4)
-        if goods_total_amount >= 2:
-            discount += 10
-            invoice_info["Discounts"]["$10 of shipping"] = "-$10"
-        if has_shoes:
-            shoes_price = Goods.objects.filter(pk=6).first().goods_price
-            discount += float(shoes_price)*0.1*shoes_num
-            invoice_info["Discounts"]["10% off shoes"] = f"-${float(shoes_price)*0.1*shoes_num}"
-        if top_outfit_amount>=2 and has_jacket:
-            jacket_price = Goods.objects.filter(pk=5).first().goods_price
-            discount += float(jacket_price)*0.5
-            invoice_info["Discounts"]["50% off jacket"] = f"-${float(jacket_price)*0.5}"
-        all_price = float(total_price)-discount+shipping_total+VAT
+        final_discount = self.discount_oper(invoice_info, goods_total_amount, has_shoes, has_jacket, top_outfit_amount, discount, shoes_num)
+        all_price = float(total_price)-final_discount+shipping_total+VAT
         invoice_info["Subtotal"] = f"${total_price}"
         invoice_info["Shipping"] = f"${shipping_total}"
         invoice_info["VAT"] = f"${VAT}"
         invoice_info["Total"] = f"${all_price}"
         if not invoice_info["Discounts"]:
             del invoice_info["Discounts"]
-        return Response({"msg": invoice_info}, status=status.HTTP_200_OK)
-
-
+        return Response({"errmsg": invoice_info}, status=status.HTTP_200_OK)
 
 
